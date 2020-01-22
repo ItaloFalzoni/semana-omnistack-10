9152c29e867f9f1f24c35c6fa9710075ac3b3bfd
italofalzoni <italo.falzoni@gmail.com>
2020-01-21 23:58:14 -0300

    quase pronto mobile

--- web/src/App.js
+++ web/src/App.js
@@ -1,98 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import api from './services/api'
+
 import './global.css'
 import './App.css'
 import './Sidebar.css'
 import './Main.css'
 
-navigator.geolocation.getCurrentPosition
+import DevForm from './components/DevForm'
+import DevItem from './components/DevItem'
 
 function App() {
+  const [devs, setDevs] = useState([])
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs')
+      setDevs(response.data)
+    }
+    loadDevs()
+  }, [])
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data)
+
+    setDevs([...devs, response.data])
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div class="input-block">
-            <label htmlFor="github_username">Usuário do GitHub</label>
-            <input name="github_username" id="github_username" required/>
-          </div>
-
-          <div class="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required/>
-          </div>
-
-          <div className="input-group">
-            <div class="input-block">
-              <label htmlFor="latitude">latitude</label>
-              <input name="latitude" id="latitude" required/>
-            </div>
-
-            <div id="input-nomargin" class="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required/>
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
 
       <main>
         <ul>
-          <li class="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/49489546?v=4" alt=""/>
-              <div class="user-info">
-                <strong>Ítalo Falzoni</strong>
-                <span>React Native</span>
-              </div>
-            </header>
-
-            <p>Biografia</p>
-            <a href="https://github.com/ItaloFalzoni">GitHub</a>
-          </li>
-
-          <li class="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/49489546?v=4" alt=""/>
-              <div class="user-info">
-                <strong>Ítalo Falzoni</strong>
-                <span>React Native</span>
-              </div>
-            </header>
-
-            <p>Biografia</p>
-            <a href="https://github.com/ItaloFalzoni">GitHub</a>
-          </li>
-
-          <li class="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/49489546?v=4" alt=""/>
-              <div class="user-info">
-                <strong>Ítalo Falzoni</strong>
-                <span>React Native</span>
-              </div>
-            </header>
-
-            <p>Biografia</p>
-            <a href="https://github.com/ItaloFalzoni">GitHub</a>
-          </li>
-
-          <li class="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/49489546?v=4" alt=""/>
-              <div class="user-info">
-                <strong>Ítalo Falzoni</strong>
-                <span>React Native</span>
-              </div>
-            </header>
-
-            <p>Biografia</p>
-            <a href="https://github.com/ItaloFalzoni">GitHub</a>
-          </li>
-
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
